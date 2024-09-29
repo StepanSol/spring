271123cb79c671744a61b9cc999c3924ced82b35
Stepan <stepan.solovev1994@gmail.com>
2024-09-29 19:31:47 +0300

    В OrderController добавлены методы поиска Order по id, поиска всех Order, метод pay переделан для работы с entity. В OrderService добавлены обслуживающие методы. Модели Order и Payment переделаны в entity. Добавлен PaymentRepository.

--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -2,21 +2,27 @@ package com.example.service;
 
 import com.example.model.CreateOrderDTO;
 import com.example.model.Order;
-import com.example.model.OrderAndPaymentDTO;
 import com.example.model.Payment;
+import com.example.model.PaymentDTO;
 import com.example.repository.OrderRepository;
+import com.example.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final PaymentRepository paymentRepository;
 
     /**
      * Название компанни, принявшей заказ
@@ -61,14 +67,51 @@ public class OrderService {
     }
 
     @Transactional
+    public void payById(UUID orderId, List<PaymentDTO> paymentsDTO){
+        Optional<Order> orderOptional = Optional.ofNullable(orderRepository.findById(orderId)
+                .orElseThrow(() -> new NoSuchElementException(String.valueOf(orderId))));
+        Order order = orderOptional.get();
+        for (PaymentDTO paymentDTO:paymentsDTO) {
+            Payment payment = Payment.builder()
+                    .creationTime(paymentDTO.getCreationTime())
+                    .sum(paymentDTO.getSum())
+                    .order(order)
+                    .build();
+            if (payment.getCreationTime().isAfter(order.getCreateTime())&&
+                    payment.getCreationTime().isBefore(order.getDeadLineOfOrder().atTime(23,59,59))&&
+                    order.getSum().compareTo(order.getSumToPay())<0
+            ){
+                order.setSum(order.getSum().add(payment.getSum()));
+                order.setNumberOfPayments(order.getNumberOfPayments()+1);
+                paymentRepository.save(payment);
+            }
+            if (order.getSum().compareTo(order.getSumToPay())>0){
+                order.setChange(order.getSum().subtract(order.getSumToPay()));
+            }
+        }
+        orderRepository.save(order);
+    }
+
+
+    @Transactional
     public Order createOrder(CreateOrderDTO dto) {
         Order order = Order.builder()
                 .companyName(companyName)
                 .createTime(LocalDateTime.now())
                 .deadLineOfOrder(dto.getDeadLineOfOrder())
+                .sum(BigDecimal.ZERO)
                 .sumToPay(dto.getSumToPay())
+                .change(BigDecimal.ZERO)
                 .clientType(dto.getClientType())
                 .build();
         return orderRepository.save(order);
+    }
+
+    public Order getOrderById (UUID id){
+        return orderRepository.findById(id).get();
+    }
+
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
     }
 }
